57e49e9f605613732207c2d93b83381675b32bc2
Ndaboom <ndabosam084@gmail.com>
2021-11-30 20:31:54 +0100

    Transalations keys and values added, users list updated

--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -3,12 +3,12 @@
 <div class="uk-card">
     <div class="row">
         <div class="col-lg-12 margin-tb">
-            <div class="uk-text-center">
-                <h2 class="uk-card-header uk-text-uppercase">Role Management</h2>
+            <div class="uk-text-center" style="padding-top: 10px;">
+                <h3 class="uk-card-header uk-text-uppercase" style="padding-left: 25px;">Role Management</h3>
             </div>
             @can('role-create')
             <div class="uk-text-right">
-                <a class="uk-button uk-button-primary uk-button-large" href="{{ route('roles.create') }}"> Create
+                <a class="uk-button uk-button-primary uk-button-large" href="{{ route('roles.create') }}" style="padding-left: 25px;"> Create
                     New
                     Role</a>
             </div>
@@ -24,11 +24,11 @@
     @endif
 
 
-    <table class="uk-table uk-table-striped">
+    <table class="uk-table uk-table-striped roles_table">
         <tr>
             <th>No</th>
             <th>Name</th>
-            <th width="280px">Action</th>
+            <th>Action</th>
         </tr>
         @foreach ($roles as $key => $role)
         <tr>
@@ -38,10 +38,10 @@
                 <div class="uk-button-group">
                     <a class="uk-button uk-button-default uk-button-mini" href="{{ route('roles.show', $role->id) }}">Show</a>
                     @can('role-create')
-                    <a class="uk-button uk-button-primary uk-button-mini" href="{{ route('roles.edit', $role->id) }}">Edit</a>
+                    <a class="uk-button uk-button-primary uk-button-miniG" href="{{ route('roles.edit', $role->id) }}">Edit</a>
 
                     {{ Form::open(['method' => 'DELETE', 'route' => ['roles.destroy', $role->id], 'style' => 'display:inline-block']) }}
-                    {{ Form::submit('Delete', ['class' => 'uk-button uk-button-danger uk-button-mini']) }}
+                    {{ Form::submit('Delete', ['class' => 'btn btn-danger btn-xs']) }}
                     {{ Form::close() }}
                     @endcan
                 </div>
